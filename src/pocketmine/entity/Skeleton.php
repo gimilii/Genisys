d8e500648cbbf3edf520f53756f7d16e1c32e000
Darin Ferguson <dferguso@cisco.com>
2016-12-22 18:55:06 -0500

    Add shooting arrow support

--- src/pocketmine/entity/Skeleton.php
+++ src/pocketmine/entity/Skeleton.php
@@ -26,11 +26,19 @@ use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\Player;
 use pocketmine\network\protocol\MobEquipmentPacket;
 use pocketmine\item\Item as ItemItem;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\FloatTag;
+use pocketmine\nbt\tag\ShortTag;
+use pocketmine\event\entity\EntityShootBowEvent;
+
 
 class Skeleton extends Monster implements ProjectileSource{
 	const NETWORK_ID = 34;
 
 	public $dropExp = [5, 5];
+	protected $startAction = -1;
 	
 	public function getName() : string{
 		return "Skeleton";
@@ -60,5 +68,42 @@ class Skeleton extends Monster implements ProjectileSource{
 		$pk->selectedSlot = 0;
 
 		$player->dataPacket($pk);
+	}
+	
+	public function onUpdate($currentTick){
+		$this->launchArrow();
+		parent::onUpdate($currentTick);
+	}
+	
+	private function launchArrow() {
+        $bow = new ItemItem(262);
+		$nbt = new CompoundTag("", [
+				"Pos" => new ListTag("Pos", [
+						new DoubleTag("", $this->x),
+						new DoubleTag("", $this->y + $this->getEyeHeight()),
+						new DoubleTag("", $this->z)
+				]),
+				"Motion" => new ListTag("Motion", [
+						new DoubleTag("", -sin($this->yaw / 180 * M_PI) * cos($this->pitch / 180 * M_PI)),
+						new DoubleTag("", -sin($this->pitch / 180 * M_PI)),
+						new DoubleTag("", cos($this->yaw / 180 * M_PI) * cos($this->pitch / 180 * M_PI))
+				]),
+				"Rotation" => new ListTag("Rotation", [
+						new FloatTag("", $this->yaw),
+						new FloatTag("", $this->pitch)
+				]),
+				]);
+		
+// 		$diff = ($this->server->getTick() - $this->startAction);
+// 		$p = $diff / 20;
+// 		$f = min((($p ** 2) + $p * 2) / 3, 1) * 2;
+// 		$ev = new EntityShootBowEvent($this, $bow, Entity::createEntity("Arrow", $this->chunk, $nbt, $this, $f == 2 ? true : false), $f);
+		$ev = new EntityShootBowEvent($this, $bow, Entity::createEntity("Arrow", $this->chunk, $nbt, $this, true), 2);
+// 		if($f < 0.1 or $diff < 5){
+// 			$ev->setCancelled();
+// 		}
+		
+		$this->server->getPluginManager()->callEvent($ev);
+		
 	}
 }
